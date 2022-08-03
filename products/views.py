07ba22f610c914128af20a2edc7361248ab1750c
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-03 13:21:49 +0000

    added new product details views

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from . models import Product, Category
+from . models import Product, Category, ProductPrice, ProductType
 
 
 def all_products(request):
@@ -71,9 +71,13 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    product_type = get_object_or_404(ProductType)
+    product_price = get_object_or_404(ProductPrice)
 
     context = {
         'product': product,
+        'product_type': product_type,
+        'product_price': product_price,
     }
 
     return render(request, 'products/product_detail.html', context)
