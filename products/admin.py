ecb987fb3fd7d1ddcae7d50cdbecc8a02a049a59
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-06 19:06:32 +0000

    changed admin panel to fit new models

---
+++ products/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+
+from products.models import Category, Combination, Product, ProductPrice, ProductType
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+class CombinationAdmin(admin.ModelAdmin):
+    list_display = (
+        "option",
+        "category",
+        "sku",
+        "name",
+        "size",
+        "description",
+        "rating",
+        "colour",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+class ProductPriceAdmin(admin.ModelAdmin):
+    list_display = ("product_price",)
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ("product_type",)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Combination, CombinationAdmin)
+admin.site.register(Category)
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(ProductPrice, ProductPriceAdmin)
