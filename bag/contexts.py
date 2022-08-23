88bec5c2681c32563a8d7b093ec12c0da81802dd
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-23 16:45:12 +0000

    added quantity input +/- buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from products.models import Combination, ProductType
+from products.models import Combination
 
 
 def bag_contents(request):
@@ -11,9 +11,6 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         combination = get_object_or_404(Combination, pk=item_id)
-        # all_prodcuts_for_purchase = Combination.objects.filter(
-        #     sku=combination.sku).filter(option__in=ProductType.objects.filter(
-        #         product_type__in=quantity))
         total += quantity * combination.option.product_price.product_price
         product_count += quantity
         bag_items.append({
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from products.models import Combination, ProductPrice, ProductType
+from products.models import Combination
 
 # Create your views here.
 
@@ -14,14 +14,10 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product_types = request.POST.get('product_types')
+    quantity = int(request.POST.get('quantity'))
     sku = request.POST.get('sku')
     price = request.POST.get('product_price')
     redirect_url = request.POST.get('redirect_url')
-    print('--startofpostrequest')
-    print(request.POST)
-    print(f'sku = {sku}')
-    print(f'product_types = {product_types}')
-    print(f'price = {price}')
     option = 0
     if product_types == 'STANDARD':
         option = 1
@@ -36,17 +32,10 @@ def add_to_bag(request, item_id):
     combination = combination[0]
     bag = request.session.get('bag', {})
 
-    # if this product cobination does not exists, then create a new sub bag
-    # if not bag.get(item_id):
-    #     bag[item_id] = {}
-
-    # bag[item_id][product_types] = True
-    # bag[item_id][price] = price
     if str(combination.id) in list(bag.keys()):
-        # if quantity += quauntity
-        bag[str(combination.id)] += 1
+        bag[str(combination.id)] += quantity
     else:
-        bag[combination.id] = 1
+        bag[combination.id] = quantity
 
     request.session['bag'] = bag
     print(request.session['bag'])
