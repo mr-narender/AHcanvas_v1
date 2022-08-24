3210a1128a6a84c65e510d454982daa59d4c63c1
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-24 17:30:35 +0000

    added stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LDVb1C1Fy8eKc05gVtKqwhiUI71mBW06gCdEHBcU3dxTzU5Iky2lEAaHO8F6bj7INyKYBWLi5ViZ36XffqwnZ5w00mI2Jz2Cs',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
