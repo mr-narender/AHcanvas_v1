7d8e14b210dc0d4839ec1e94f6e382aa25de3d29
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-27 22:10:23 +0000

    added product admin view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, re_path
 from products import views
 
 urlpatterns = [
-    path("detail/<product_sku>", views.product_detail, name="product_detail"),
-    re_path(r"^(page/(?P<page>\d+))?/$", views.all_products, name="products"),
+    path("add/", views.add_product, name="add_product"),
+    path("int:detail/<product_sku>/", views.product_detail, name="product_detail"),
+    re_path(r"^(page/(?P<page>\d+))?/$/", views.all_products, name="products"),
     path(r"", views.all_products, name="products"),
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 
 from products.models import Category, Combination, Product
+from .forms import ProductForm
 from products.utils import paginateProducts
 
 
@@ -93,3 +94,14 @@ def product_detail(request, product_sku):
     }
 
     return render(request, "products/product_detail.html", context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_products.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
