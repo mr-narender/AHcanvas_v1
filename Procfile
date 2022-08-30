cc3ad556ac00953f6d59bc0c1cd104c7f7445ac5
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-08-30 14:05:41 +0000

    heroku set up deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ahcanvas.wsgi:application
--- ahcanvas/settings.py
+++ ahcanvas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-$(0$^fr5ax1dkrxlm*xff24gp)ioeiz1vza&2q8!df14@_b8*a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ahcanvas.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
