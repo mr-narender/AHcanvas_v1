b7b631d02944d74dd6cff2acca31e304c9ce45cb
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-09-06 22:18:20 +0000

    code review

--- checkout/views.py
+++ checkout/views.py
@@ -77,15 +77,18 @@ def checkout(request):
                     messages.error(
                         request,
                         (
-                            "One of the products in your bag wasn't found in our database."
-                            "Please call us for assistance!"
+                            "One of the products in your bag wasn't found in"
+                            "our database. Please call us for assistance!"
                         ),
                     )
                     order.delete()
                     return redirect(reverse("view_bag"))
 
             request.session["save_info"] = "save-info" in request.POST
-            return redirect(reverse("checkout_success", args=[order.order_number]))
+            return (redirect(
+                reverse("checkout_success",
+                        args=[order.order_number]))
+                    )
         else:
             messages.error(
                 request,
@@ -95,7 +98,8 @@ def checkout(request):
     else:
         bag = request.session.get("bag", {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request,
+                           "There's nothing in your bag at the moment")
             return redirect(reverse("products"))
 
         current_bag = bag_contents(request)
