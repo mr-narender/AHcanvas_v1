b6577e74102b969fe337a8bdfa64928e56362495
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-10-03 13:49:14 +0000

    added aws expiary date

--- ahcanvas/settings.py
+++ ahcanvas/settings.py
@@ -177,6 +177,7 @@ USE_TZ = True
 if os.environ.get('USE_AWS'):
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
 
